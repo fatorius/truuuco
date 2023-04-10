d673c06a8b19cd3e77f5c2f0afe879de9ef1e19b
Hugo Souza <hugo.souza0610@gmail.com>
2023-04-10 19:29:47 -0400

    Adiciona mensagem de inicialização

--- src/init.cpp
+++ src/init.cpp
@@ -1,8 +1,20 @@
+#include <iostream>
+
 #include "init.h"
 #include "interface.h"
 #include "state.h"
 
+using namespace std;
+
+void printInitMessage(){
+    cout<<"C++ Truuuco 2023"<<endl;
+    cout<<"Criado por HugoSouza"<<endl;
+    cout<<"Algoritmo pronto"<<endl;
+}
+
 void init(){
     initCommands();
     initState();
+
+    printInitMessage();
 }
